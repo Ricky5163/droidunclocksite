97470315d42e47fb7104d0c52ef6474f53718526
Ricardo <liedsondias1957@gmail.com>
2026-04-25 13:40:25 +0200

    Point frontend to new Supabase project

--- js/app-config.js
+++ js/app-config.js
@@ -1,6 +1,6 @@
 const runtimeConfig = window.DROIDUNCLOCK_CONFIG || {};
 
-const SUPABASE_URL = runtimeConfig.SUPABASE_URL || "https://eqklkfrxotoizpuacznc.supabase.co";
+const SUPABASE_URL = runtimeConfig.SUPABASE_URL || "https://rtjfezznepfshhrpvjcf.supabase.co";
 const SUPABASE_ANON_KEY =
   runtimeConfig.SUPABASE_ANON_KEY ||
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxa2xrZnJ4b3RvaXpwdWFjem5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMDAxMTAsImV4cCI6MjA4NTg3NjExMH0.Ex1LHdLN8Kfnu3ySY1JH7NUC9AM-TqXLnBiA56qE9Ow";
